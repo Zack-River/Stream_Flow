4d91c70afd0c93d25f1fe13c0c1ded5d09a579cb
mohamed zakaria <you@example.com>
2025-07-16 13:17:46 +0300

    audio model , controller , multer , routes

---
+++ models/addio.Models.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const User = require("../models/user.Model")
+const audioSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  genre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  audioUrl: {
+    type: String,
+    required: true
+  },
+  coverImageUrl: {
+    type: String,
+    required: true
+  },
+  isPrivate: {
+    type: Boolean,
+    default: false
+  },
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Audio = mongoose.model("Audio", audioSchema);
+
+module.exports = Audio;
