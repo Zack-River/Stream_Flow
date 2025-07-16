ae602d116c99f240ff27bf2d1b0d823cb951a8b9
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-07-16 10:12:26 +0300

    User Model , Routes , Controllers and middleware

---
+++ routes/user.Routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.Controller')
+const authController = require('../controllers/auth.Controller');
+const {checkAuthenticated , authorizeRoles} = require('../middlewares/auth.Middleware');
+
+// admin routes
+router.get('/admin/users' , checkAuthenticated, authorizeRoles('admin'), userController.getUsers);
+router.post('/admin/user' , checkAuthenticated, authorizeRoles('admin'), userController.createUser);
+
+// user routes
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.post('/forget-password', authController.forgetPassword);
+router.post('/reset-password', authController.resetPassword);
+router.post('/logout', authController.logout);
+router.get('/profile', checkAuthenticated, authController.showProfile);
+router.get('/:username' , checkAuthenticated, userController.getUser);
+
+module.exports = router;
---
+++ utils/cookie.js
@@ -0,0 +1,44 @@
+// exports.setAuthCookies = function (res, accessToken, refreshToken) {
+//   createCookie(res, 'accessToken', accessToken, 60 * 60 * 1000); // 1 hour
+//   createCookie(res, 'refreshToken', refreshToken, 7 * 24 * 60 * 60 * 1000); // 7 days
+// };
+
+exports.setAccessTokenCookie = function (res, accessToken) {
+  createCookie(res, 'accessToken', accessToken, 60 * 60 * 1000); // 1 hour
+};
+
+exports.setRefreshTokenCookie = function (res, refreshToken) {
+  createCookie(res, 'refreshToken', refreshToken, 7 * 24 * 60 * 60 * 1000); // 7 days
+};
+
+exports.setResetCookie = function (res, resetToken) {
+  createCookie(res, 'resetToken', resetToken, 10 * 60 * 1000); // 10 minutes
+};
+
+exports.clearResetCookie = function (res) {
+  removeCookie(res, 'resetToken');
+};
+
+exports.clearAuthCookies = function (res) {
+  removeCookie(res, 'accessToken');
+  removeCookie(res, 'refreshToken');
+};
+
+function createCookie(res, name, token, maxAge) {
+  res.cookie(name, token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production', // Only HTTPS in prod
+    sameSite: 'strict',          // Prevent CSRF
+    maxAge: maxAge,
+    path: '/',                   // Available to entire app
+  });
+}
+
+function removeCookie(res, name) {
+  res.clearCookie(name, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    path: '/',
+  });
+}
