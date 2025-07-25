0dadacea435a50eca2e08a08d5ba165e9462b6a4
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-07-25 19:15:29 +0300

    Project Planner File added

--- public/assets/js/save-current-song.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("beforeunload", () => {
-  localStorage.setItem("currentSongSrc", footerAudio.src);
-  localStorage.setItem("currentSongTitle", playerTitle.textContent);
-  localStorage.setItem("currentSongArtist", playerArtist.textContent);
-  localStorage.setItem("currentSongCover", playerCover.src);
-  localStorage.setItem("currentSongTime", footerAudio.currentTime);
-  localStorage.setItem("isPlaying", !footerAudio.paused);
-});
--- server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-const app = require('./app');
-
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT , () => {
-    console.log('Server is running on port: ' + PORT);
-});
