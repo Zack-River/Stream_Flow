3f9a7c9582563b229516f6c6ea23ef3b167e9d37
Abdallah Wageeh(Zack River) <111385589+Zack-River@users.noreply.github.com>
2025-08-05 00:13:37 +0300

    Stream Flow API v1.0.3

--- server/app.js
+++ server/app.js
@@ -29,8 +29,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Static assets first
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "uploads")));
 
 // Documentation markdown route
 app.get("/", (req, res) => {
